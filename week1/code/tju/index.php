9b750fc8a9ee60348a174cf12635c2c3258e3153
Cornejo, Luis <Luis.Cornejo@ellucian.com>
2019-08-28 16:35:20 -0400

    Added new code

---
+++ week1/code/tju/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<header>
+    <title>This is title</title>
+</header>
+
+<body>
+    <?php
+        $message = 'Hello World!';
+        echo $message;
+    ?>
+</body>
+</html>
