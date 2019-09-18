7256815a7799a8ca8729229e415c3ade5beec19d
Cornejo, Luis <Luis.Cornejo@ellucian.com>
2019-09-18 19:06:04 -0400

    ANother change.

---
+++ week4/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+        table, td, th {
+            border: 1px solid black;
+        }
+        td, th {
+            padding: 10px;
+        }
+        .red, td {
+            color: red;
+        }
+        .green, td {
+            color: green;
+        }
+    </style>
+</head>
+<body>
+<h1>
+Hello Another Change
+</h1>
+
+</body>
+</html>
